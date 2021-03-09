85476ef4c6b540eb0936daf825e4c18349c14731
Jackie Sun <jackie@Jackies-MacBook-Pro.local>
2021-03-09 16:46:20 +1100

    Added hamburger slide down menu.
    Added white background when hambuger menu is clicked, changed color of text in hamburger menu to black

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const links = document.querySelector(".hamburger-links");
-// const backgroundSlider = document.querySelector(".main-overlay");
+const backgroundSlider = document.querySelector(".main-overlay");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   const isActive = hamburger.classList.contains("is-active");
   if (isActive) {
     links.style.display = "block";
-    // backgroundSlider.style.display = "block";
+    backgroundSlider.style.display = "block";
   } else {
     links.style.display = "none";
+    backgroundSlider.style.display = "none";
   }
 });
